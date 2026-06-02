425ec68042234e0a9a7e7c2a8f2bf28001aa63ad
Stefanie Klaic <101341717+SteffiKlaic@users.noreply.github.com>
2026-06-03 00:55:14 +0200

    Initial ASP.NET Web API setup

---
+++ Controllers/DogController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DogBreedAi.Controllers
+{
+    [ApiController]
+    [Route("api/dogs")]
+    public class DogController : Controller
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok(new
+            {
+                Breed = "Golden Retriever",
+                Description = "Friendly and intelligent"
+            });
+        }
+    }
+}
